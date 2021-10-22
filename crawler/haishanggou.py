b8f34217f0945fa65e832775325b2fef7015a562
heisenberg000 <heisenberg001@163.com>
2021-10-22 17:24:01 +0800

    完善大图切割功能

--- crawler/haishanggou.py
+++ crawler/haishanggou.py
@@ -14,11 +14,12 @@ import requests
 import hashlib
 import base64
 from PIL import Image
+import math
 
 # 公司代理
 # proxies = {
-#     "http": "http://user:password@10.185.113.100:8002",
-#     "https": "http://user:password@10.185.113.100:8002",
+#     "http": "http://@10.185.113.100:8002",
+#     "https": "http://@10.185.113.100:8002",
 # }
 
 # 自己电脑
@@ -109,10 +110,27 @@ def baidu_api(file,access_token):
         return response.json()
 
 
-def split_pic(url):
+def split_pic(url,pic_base=1024):
     '''切割图片'''
+    dir = url[:len(os.path.dirname(url))+1]
+    name,suffix = os.path.splitext(url[len(os.path.dirname(url))+1:])
     img = Image.open(url)
-    print(img.size)
+    w,h = img.size
+    if w < 15:
+        print(f'图片宽度{w}px，至少大于15px')
+        return None
+    if h > pic_base:
+        # 循环次数
+        c = math.ceil(h / pic_base)
+        for i in range(c):
+            x1,x2 = 0,w
+            y1 = i*pic_base
+            y2 = h if (i+1)*pic_base > h else (i+1)*pic_base
+            cropim = img.crop((x1,y1,x2,y2))
+            cropim.save(dir+name+str(i).rjust(3,'0')+suffix)
+        print(f'切割完成：{name}{suffix}共切割为{c}个子文件')
+        print(f'删除文件：{name}{suffix}')
+        os.remove(url)
 
 def main(folder):
     '''获取完整获奖名单清单'''
@@ -124,40 +142,23 @@ def main(folder):
         pic_urls = parse_html(html.decode(encoding='utf-8'))      
         count = download_pic(pic_urls,folder)
         if count > 0:
-            parse_pic(folder)
-
-'''
-Pillow 测试代码
-from PIL import Image, ImageEnhance
-image = Image.open('1.jpg')
-w,h = image.size
-#box对背景图进行上下分割。
-box1 = (0,0,w,int(h/2))
-box2 = (0,int(h/2),w,h)
-#im1是上半部分
-#im2是下半部分
-im1 = image.crop(box1)
-im2 = image.crop(box2)
-#新建画布
-flag = Image.new('RGB',(w,h))
-#对图片上半部分进行明暗处理，对下半部分进行对比度处理
-im1 = ImageEnhance.Brightness(im1).enhance(0.5)
-im2 = ImageEnhance.Contrast(im2).enhance(2.0)
-flag.paste(im1, (0,0))
-flag.paste(im2, (0,int(h/2)))
-flag.save('result.jpg',quality=100)
-flag.show()
-'''
+            for file in os.listdir(folder):
+                split_pic(folder+file)
+            #parse_pic(folder)
 
 
 
 if __name__ == '__main__':
     #folder = 'E:\\Python\\liaoxuefeng\\pachong\\haishang\\third\\'
-    root_folder = 'G:\\GitWorkSpace\\python_practice\\crawler\\'
-    folder = 'haishang\\third\\'
-    #main(root_folder + folder)
+    #家庭电脑目录
+    #root_folder = 'G:\\GitWorkSpace\\python_practice\\crawler\\'
+    #工作电脑目录
+    # root_folder = 'E:\\Gitworkspace\\python_practice\\crawler\\'
+    # folder = 'haishang\\third\\'
+    # main(root_folder + folder)
     #print(get_token())
-    # access_token = get_token()
-    # print(f'token：{access_token}')
-    # baidu_api('G:\\GitWorkSpace\\python_practice\\crawler\\haishang\\third\\2a4916e3c7f6851413edb3cacbab992a.png',access_token)
-    split_pic('G:\\GitWorkSpace\\python_practice\\crawler\\haishang\\third\\2a4916e3c7f6851413edb3cacbab992a.png')
+    access_token = get_token()
+    print(f'token：{access_token}')
+    baidu_api('E:\\Gitworkspace\\python_practice\\crawler\\haishang\\third\\7e5ca15bc3c550e05cd47f7a339b1f44008.png',access_token)
+    # split_pic('G:\\GitWorkSpace\\python_practice\\crawler\\haishang\\third\\2a4916e3c7f6851413edb3cacbab992a.png')
+    
